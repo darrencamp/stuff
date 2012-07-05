41bfb686a6ceb376b62312361c65bbdee4a9e663
Darren Camp <darren@codingintent.com>
2012-07-05 21:59:09 +0930

    added loan and borrowing counter to the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
       @loans = Loan.where('returned_date is null')
       @loans.where('user_id = ?', current_user.id)
       @loans.all
+
+      @borrowing = Borrower.where('email = ?', current_user.email)
+      @borrowing.all
     end
   end  
   
