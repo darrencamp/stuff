c7169f5371db7e1cd78c4a50930dcf57498c1310
Darren Camp <darren@codingintent.com>
2012-07-12 16:40:56 +0930

    using id's instead of classes to identify the loan and borrowing counters

--- features/step_definitions/loan_steps.rb
+++ features/step_definitions/loan_steps.rb
@@ -1,11 +1,11 @@
 Then /^the loan counter should be (\d+)$/ do |arg1|
-  within('.counter .loan') do
+  within('#loan-counter') do
     page.should(have_content(arg1))
   end
 end
 
 Then /^the borrowing counter should be (\d+)$/ do |arg1|
-  within('.counter .borrowing') do
+  within('#borrowing-counter') do
     page.should(have_content(arg1))
   end
 end
