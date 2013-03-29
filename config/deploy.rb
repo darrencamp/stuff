89d623e6aaedabf4f589cb1a0b26a07bbad0c1af
N.T. Tuddenham <ferrisoxide@gmail.com>
2013-03-29 19:15:36 +1030

    installs code, not running yet

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,12 @@
 # NOTE Move require "bundler/capistrano" to end of file if using 'set :bundle_roles'  
 require "bundler/capistrano"
 
-#load 'config/recipes/rvm'
+set :stages, %w(production staging)
+set :default_stage, "staging"
 
-set :application, "stuff"
+require 'capistrano/ext/multistage'
+
+#load 'config/recipes/rvm'
 
 # Git-related settings
 default_run_options[:pty] = true  # Must be set for the password prompt
@@ -16,35 +19,55 @@ set :ssh_options, { :forward_agent => true }
 
 set :scm, "git"
 set :repository,  "https://github.com/darrencamp/stuff"
-set :branch, "develop" # TODO Change this 'master' when we start rolling production releases
 
 set :use_sudo, false
-set :user, "tom" # TODO Need to change this to a 'deploy' user or similar
+set :user, "stuff" # TODO Need to change this to a 'deploy' user or similar
 set :deploy_via, :remote_cache
-
+set :ssh_options, { :forward_agent => true }
+set(:deploy_to){ "/home/#{user}/web/#{rails_env}"}
 set :keep_releases, 5
+
+before "deploy:assets:precompile", 'deploy:symlink_shared'
+
+namespace :deploy do
+
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+
+
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+end
+
+after "deploy", "rvm:trust_rvmrc"
+#before 'deploy:setup', 'rvm:install_ruby' 
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-# TODO Need proper domain names
-role :web, "198.58.106.171"                          # Your HTTP server, Apache/etc
-role :app, "198.58.106.171"                          # This may be the same as your `Web` server
-role :db,  "198.58.106.171", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+# # TODO Need proper domain names
+# role :web, "198.58.106.171"                          # Your HTTP server, Apache/etc
+# role :app, "198.58.106.171"                          # This may be the same as your `Web` server
+# role :db,  "198.58.106.171", :primary => true # This is where Rails migrations will run
+# #role :db,  "your slave db-server here"
 
-set :deploy_to, "/var/www/#{application}"
+
 
 # SMELL dodgey town
-namespace :deploy do
-  desc "Create base application folder"
-  task :create_app_dir do
-    run "#{sudo} mkdir -p /var/www/#{application}"
-    run "#{sudo} chown -R tom /var/www/#{application}"
-  end
-end
-    
-before "deploy:setup", "deploy:create_app_dir"
+# namespace :deploy do
+#   desc "Create base application folder"
+#   task :create_app_dir do
+#     run "#{sudo} mkdir -p /var/www/#{application}"
+#     run "#{sudo} chown -R tom /var/www/#{application}"
+#   end
+# end
+#     
+# before "deploy:setup", "deploy:create_app_dir"
 
 # namespace :deploy do
 #   desc "Install everyitem onto the server"
@@ -75,3 +98,5 @@ before "deploy:setup", "deploy:create_app_dir"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+# require File.expand_path(__FILE__) + '/../recipes/rvm'
