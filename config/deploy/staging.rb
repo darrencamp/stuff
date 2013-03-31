6214c09cf4acc57a6fab65f75209b447813c1ad0
N.T. Tuddenham <ferrisoxide@gmail.com>
2013-03-31 12:46:00 +1030

    modifying staging unicorn config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,6 +4,7 @@ set :branch, "develop"
 server 'stuff.staging.eventidesystems.com', :app, :web, :primary => true
 
 set :rails_env, 'staging'
+set :unicorn_env, 'staging'
 
 set :rvm_ruby_string, '1.9.3@stuff-staging'
 set :rvm_type, :user
