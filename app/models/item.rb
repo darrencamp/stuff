b8aa0bb06eae279e6992410acabfb4a8e1764705
Darren Camp <darren@codingintent.com>
2013-04-06 16:06:30 +1030

    first implementation of tagging using the acts-as-taggable-on gem

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
-  attr_accessible :name, :description
+  attr_accessible :name, :description, :tag_list
   belongs_to :user
+
+  acts_as_taggable
   
   validates_uniqueness_of :name, :scope => :user_id
   validates_presence_of :user 
