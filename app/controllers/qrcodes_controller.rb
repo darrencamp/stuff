cc4fd7d172e45119d45567f17206a19e702a1114
Darren Camp <darren@codingintent.com>
2013-06-20 22:27:49 +0930

    list all bucket qrcodes

--- app/controllers/qrcodes_controller.rb
+++ app/controllers/qrcodes_controller.rb
@@ -1,7 +1,10 @@
 class QrcodesController < Base::AuthenticatedController
 
   def index
+    @buckets = current_user.buckets.all #SMELL: paginate
+    
     respond_to do |format|
+      format.html # show all buckets
       format.svg { render :qrcode => params[:path], :level => :h, :unit => 4 }
     end
   end
